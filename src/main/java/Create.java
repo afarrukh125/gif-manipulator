9701f40b5ec525ce751ce44d2e38e0f38f862abe
Abdullah Farrukh <33581953+afarrukh125@users.noreply.github.com>
2023-03-01 07:55:04 +0000

    Improve create command significantly

--- src/main/java/Create.java
+++ src/main/java/Create.java
@@ -2,11 +2,17 @@ import at.dhyan.open_imaging.GifDecoder;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Runtime.getRuntime;
+import static java.util.concurrent.Executors.newFixedThreadPool;
 
 /**
  * Uses the Open-Imaging library by DhyanB: https://github.com/DhyanB/Open-Imaging
@@ -20,28 +26,49 @@ public class Create {
      * @throws IOException If file not found or similar
      */
     public static void main(String[] args) throws IOException {
-        String path = "";
+        String path = null;
         if(args.length == 0) {
-            JFileChooser chooser = new JFileChooser();
-            chooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+            setupUILookAndFeel();
+            JFileChooser chooser = setupFileChooser();
             int result = chooser.showOpenDialog(null);
             if(result == JFileChooser.APPROVE_OPTION) {
-                File selectedFile = chooser.getSelectedFile();
-                if(!selectedFile.getName().contains(".gif")) {
-                    JOptionPane.showMessageDialog(null, "Please select a GIF.");
-                    System.exit(0);
-                } else
-                    path = selectedFile.getAbsolutePath();
+                path = chooser.getSelectedFile().getAbsolutePath();
             }
         }
-        else
+        else {
             path = args[0];
-        final FileInputStream data = new FileInputStream(path);
-        String newLocation = new File(path).getName().replace(".gif", "");
-        writeImages(data, newLocation);
+        }
 
-        JOptionPane.showMessageDialog(null, "Converted " + new File(path).getName() +
-                " to images successfully! Check the new directory.");
+        System.out.println(path);
+        if(path == null) {
+            throw new IllegalArgumentException("No path provided, exiting...");
+        }
+        final FileInputStream data = new FileInputStream(path);
+        String finalLocation = "target/" + new File(path).getName().replace(".gif", "");
+        writeImages(data, finalLocation);
+
+        String outputAbsolutePath = new File(finalLocation).getAbsolutePath();
+//        JOptionPane.showMessageDialog(null, "Converted " + new File(path).getName() +
+//                                            " to images successfully! Check the files in directory " + outputAbsolutePath + ".");
+    }
+
+    private static JFileChooser setupFileChooser() {
+        JFileChooser chooser = new JFileChooser();
+        File initialFolder = new File(System.getProperty("user.home"));
+        chooser.setCurrentDirectory(initialFolder);
+        chooser.setAcceptAllFileFilterUsed(false);
+        FileNameExtensionFilter fileFilter = new FileNameExtensionFilter("GIF files", "gif");
+        chooser.addChoosableFileFilter(fileFilter);
+        return chooser;
+    }
+
+    private static void setupUILookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | UnsupportedLookAndFeelException | IllegalAccessException |
+                 InstantiationException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -51,13 +78,43 @@ public class Create {
      * @throws IOException If file not found or similar
      */
     private static void writeImages(FileInputStream data, String outFileName) throws IOException {
+        writeParallel(data, outFileName);
+    }
+
+    private static void writeParallel(FileInputStream data, String outFileName) throws IOException {
+        final GifDecoder.GifImage gif = GifDecoder.read(data);
+        final int frameCount = gif.getFrameCount();
+        ExecutorService executorService =  newFixedThreadPool(getRuntime().availableProcessors() * 2);
+        for (int i = 0; i < frameCount; i++) {
+            BufferedImage img = gif.getFrame(i);
+            int finalI = i;
+            executorService.execute(() -> outputFrame(outFileName, finalI, img));
+        }
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(5, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void outputFrame(String outFileName, int index, BufferedImage img) {
+        File file = new File(outFileName + "/" + index + ".png");
+        if (file.mkdirs()) {
+            try {
+                ImageIO.write(img, "png", file);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static void writeSingleThreaded(FileInputStream data, String outFileName) throws IOException {
         final GifDecoder.GifImage gif = GifDecoder.read(data);
         final int frameCount = gif.getFrameCount();
         for (int i = 0; i < frameCount; i++) {
             BufferedImage img = gif.getFrame(i);
-            File file = new File(outFileName + "/" + i + ".png");
-            if (file.mkdirs())
-                ImageIO.write(img, "png", file);
+            outputFrame(outFileName, i, img);
         }
     }
 
